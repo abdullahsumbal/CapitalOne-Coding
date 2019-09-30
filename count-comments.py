97b5012611e4230ac1109ccaaa27c0ab853c68b2
abdullahsumbal <abdullahsumbal@gmail.com>
2019-09-30 07:15:50 -0400

    python code added

---
+++ count-comments.py
@@ -0,0 +1,78 @@
+"""
+Author: Muhammad Abdullah Sumbal
+
+count comments.
+"""
+import re
+from helper import read_file
+
+file_path = "data-samples/python-file2.py"
+total_lines = 0
+total_single_comments = 0
+total_multi_comments = 0
+total_multi_comments_lines = 0
+total_TODOs = 0
+
+
+file_lines = read_file(file_path, lines=True)
+file_content = read_file(file_path, lines=False)
+
+# total lines - When a file is checked in, scan the file to count the total number of lines.
+total_lines = len(file_lines)
+
+# total multiple line comments
+# multi_comments_list = re.findall("""("{3}[\s\S]*?"{3}|'{3}[\s\S]*?'{3})""", file_content)
+# total_multi_comments = len(multi_comments_list)
+
+quotes_found = False
+between_round_brackets = False
+is_previous_single_line_comment = False
+for index, line in enumerate(file_lines):
+
+    line, block_comments_inline = re.subn("""(?<!\(\s*)("{3}[\s\S]*?"{3}|'{3}[\s\S]*?'{3})(?![\s\S]*?\))""", "", line)
+    if not between_round_brackets:
+        total_multi_comments += block_comments_inline
+
+
+    line, block_comments_inline = re.subn("""(?<!\()[\s\S]*?("{3}[\s\S]*?|'{3}[\s\S]*?)(?![\s\S]*?\))""", "", line)
+    if "(" in line:
+        between_round_brackets = True
+    elif ")" in line:
+        between_round_brackets = False
+
+    if block_comments_inline > 0 and not between_round_brackets:
+        if quotes_found:
+            quotes_found = False
+            total_multi_comments += block_comments_inline
+        else:
+            quotes_found = True
+
+    # if "#" in line and not is_previous_single_line_comment:
+    #     total_single_comments += 1
+    #     is_previous_single_line_comment = True
+    # elif "#" not in line and is_previous_single_line_comment:
+    #     is_previous_single_line_comment = False
+    #     total_multi_comments += 1
+
+
+
+    print(between_round_brackets, line.strip(), total_multi_comments)
+
+
+
+
+    file_lines[index] = line
+        # if block_comment:
+        #     total_multi_comments+=1
+        #     block_comment = False
+        # else:
+        #     block_comment = True
+
+
+
+# for line in file_lines:
+#     print(line)
+print("total_lines:", total_lines)
+print("total_multi_comments:", total_multi_comments)
+print("total_single_comments:", total_single_comments)
+
---
+++ count-comments_char.py
@@ -0,0 +1,118 @@
+"""
+Author: Muhammad Abdullah Sumbal
+
+count comments.
+"""
+import re
+from helper import *
+
+
+
+file_path = "data-samples/python-file1.py"
+total_lines = 0
+line_num_single_comments = []
+total_single_comments = 0
+total_multi_comments = 0
+total_multi_comments_lines = 0
+total_TODOs = 0
+
+
+file_lines = read_file(file_path, lines=True)
+file_content = read_file(file_path, lines=False)
+
+# total lines - When a file is checked in, scan the file to count the total number of lines.
+total_lines = len(file_lines)
+
+
+code_mode = True
+between_brackets = False
+stack = []
+
+# Iter every line
+for line_index, line in enumerate(file_lines):
+
+    char_index = 0
+    # iter every char
+    while char_index < len(line):
+        char = line[char_index]
+
+        if char == hashtag:
+            # If there is no quotation before #, # is used as a comment.
+            if stack:
+                if not is_part_of_comment_or_string(stack):
+                    total_single_comments += 1
+                    # keep track of the line number, later used to find block comments
+                    line_num_single_comments.append(line_index)
+                    # No need to check other char in line because anything after # is part of the comment
+                    stack.append(hashtag)
+                    # break
+            # no, quotation found before #, so it is used as a comment.
+            else:
+                # keep track of the line number, later used to find block comments
+                line_num_single_comments.append(line_index)
+                total_single_comments += 1
+                stack.append(hashtag)
+                # break
+        elif char == double_quote or char == single_quote:
+            quote = char
+            # it could be a block comment so check if next two char are '""' in the line
+            if is_triple_quotes(line, char_index, quote):
+                if stack:
+                    if not is_part_of_comment_or_string(stack, quote=quote):
+                        # closing of block comment
+                        if stack[-1] == get_triple_quotes(quote):
+                            stack.pop()
+                            char_index += 2
+                            total_multi_comments += 1
+                            total_multi_comments_lines += 1
+                        # if round bracket are open then it could be used as an string argument.
+                        elif stack[-1] != "(":
+                            stack.append(get_triple_quotes(quote))
+                            char_index += 2
+                # start of block comment
+                else:
+                    stack.append(get_triple_quotes(quote))
+                    char_index += 2
+            # just a simple quotation.
+            else:
+                if stack:
+                    if stack[-1] == quote:
+                        stack.pop()
+                else:
+                    stack.append(char)
+        elif char == '(':
+            if stack:
+                # This should not be a part of a comment or string
+                if not is_part_of_comment_or_string(stack):
+                    stack.append(char)
+            else:
+                stack.append(char)
+        elif char == ")":
+            if stack:
+                if stack[-1] == '(':
+                    stack.pop()
+        elif char == "T":
+            if stack:
+                if is_part_of_comment_or_string(stack):
+                    total_TODOs += is_TODO_in_comment(line, char_index)
+
+        char_index += 1
+
+    # this check if we are in comment mode or code mode
+    if stack:
+        if is_part_of_comment_or_string(stack):
+            total_multi_comments_lines += 1
+        if stack[-1] == hashtag:
+            stack.pop()
+
+extra_block_comments, total_single_comments = count_block_comment_from_single_comments(line_num_single_comments)
+
+total_multi_comments += extra_block_comments
+
+print("total_lines:", total_lines)
+print("total_multi_comments:", total_multi_comments)
+print("total_multi_comments_lines", total_multi_comments_lines)
+print("total_single_comments:", total_single_comments)
+print("total_TODOs:", total_TODOs)
+
+
